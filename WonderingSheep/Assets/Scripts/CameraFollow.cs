8457621de525ac587de2da9f0d1a62e25e4f9f04
Boris Filipovic <boris.filipovic1@gmail.com>
2020-04-28 21:39:50 +0200

    Added camer follow player script.

---
+++ WonderingSheep/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Public.
+    public float moveSmoothing = 10f;
+    public float rotationSmoothing = 15f;
+
+    // Private.
+    private Transform target;
+    private Vector3 targetForward;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        target = GameObject.FindWithTag(Tags.PLAYER_TAG).transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetForward = transform.forward;
+        targetForward.y = 0f;
+        SnapToPlayerPosition(); // On start we want camer to snap to players position.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Snap camera to players(target) location.
+    private void SnapToPlayerPosition() {
+        // Check for null value.
+        if(target != null) {
+            transform.position = target.position;
+        }
+        Vector3 forward = targetForward;
+        forward.y = transform.forward.y;
+        transform.forward = forward;
+    }
+
+    // Camere should follow player method.
+    private void FollowPlayer() {
+        if(target != null) {
+            // Slowly move to new position.
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * moveSmoothing);
+        }
+        Vector3 forward = transform.forward;
+        forward.y = 0f;
+        forward = Vector3.Slerp(forward, targetForward, Time.deltaTime * rotationSmoothing);
+        forward.y = transform.forward.y;
+        transform.forward = forward;
+    }
+}
---
+++ WonderingSheep/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Public.
+    public float moveSpeed = 3f;
+
+    // Private.
+    private Rigidbody rigidbody;
+    private float smoothMovement = 15f;
+    private bool canMove;
+    private Vector3 targetForward;
+    private Vector3 positionChange;
+    private Camera mainCameraReference;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        targetForward = transform.forward;
+        mainCameraReference = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateForward();
+        GetInput();
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void GetInput() {
+        // Get left mouse click.
+        if (Input.GetMouseButtonDown(0)) {
+            canMove = true;
+        } else if (Input.GetMouseButtonUp(0)) {
+            canMove = false;
+        }
+    }
+
+    private void UpdateForward() {
+        transform.forward = Vector3.Slerp(transform.forward, targetForward, Time.deltaTime * smoothMovement);
+    }
+
+    private void MovePlayer() {
+        if (canMove) {
+            // Delta position. Calculate how much we need to move player.
+            positionChange = new Vector3(Input.GetAxisRaw(Axix.MOUSE_X), 0.0f, Input.GetAxisRaw(Axix.MOUSE_Y));
+            positionChange.Normalize();
+            positionChange *= moveSpeed * Time.fixedDeltaTime;
+            positionChange = Quaternion.Euler(0f, mainCameraReference.transform.eulerAngles.y, 0f) * positionChange;
+
+            // Move gameobjest - player.
+            rigidbody.MovePosition(rigidbody.position + positionChange);
+
+            if(positionChange != Vector3.zero) {
+                // Model is faced the wrong way so that is why we have negative value here.
+                targetForward = Vector3.ProjectOnPlane(-positionChange, Vector3.up);
+            }
+        }
+    }
+}
